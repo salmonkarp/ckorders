27676016f696f82b202103ca84a86b662d036689
salmonkarp <garyaxelmuliyono@gmail.com>
2024-01-14 12:07:45 +0700

    Security

--- testers.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-import os
-os.environ["MONGOKEY"] = "l6ws7zM0vFplKeTc"
-database_key = os.environ["MONGOKEY"]
-MCString = "mongodb+srv://salmonkarp:" + database_key + "@cookieskingdomdb.gq6eh6v.mongodb.net/"
-print(MCString)
-MClient = pymongo.MongoClient(MCString)['CK']
-MClient['POs'].delete_many({})
