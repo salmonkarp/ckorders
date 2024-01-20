a56f9677c353228399fde6b320b9f8f6c75cb7ab
salmonkarp <garyaxelmuliyono@gmail.com>
2024-01-20 17:28:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -74,9 +74,9 @@ def encode_pdf_as_base64(file_path):
 
 # objects creation
 app = Flask(__name__)
-app.secret_key = "bobby"
+app.secret_key = os.environ["SECRETKEY"]
 
-os.environ['MONGOKEY'] = "8EGuh1hHtHosYg5U"
+
 database_key = os.environ["MONGOKEY"]
 MCString = "mongodb+srv://salmonkarp:" + database_key + "@cookieskingdomdb.gq6eh6v.mongodb.net/"
 MClient = pymongo.MongoClient(MCString)['CK']
